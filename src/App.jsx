8e5dfdd0ebbdaf071898ee73aac5a9017dee4724
PilarGallegoS <pgsubias@gmail.com>
2024-12-04 19:18:14 +0000

    edición de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import React from "react";
+import { Container, FormControl, ListGroup } from "react-bootstrap";
+
 
 function ToDoList() {
     const [ToDo, setToDo] = useState([
@@ -21,18 +23,25 @@ function ToDoList() {
     const removeToDo = (id) => {
         setToDo(ToDo.filter((ToDo) => ToDo.id !== id));
     };
+    const [hoveredTaskId, setHoveredTaskId] = useState(null);
+
     return (
-        <div>
-            <input type="text" value={newToDo} onChange={(e) => setNewToDo(e.target.value)} onKeyDown={addToDo} placeholder="Add a new task" />
-            <ul>
+        <Container className="container mt-5">
+            <h1 className="text-center">To Do List:</h1>
+            <div className="addTask mx-auto"style={{ maxWidth: "400px" }}>
+                <FormControl type="text" value={newToDo} onChange={(e) => setNewToDo(e.target.value)} onKeyDown={addToDo} placeholder="Add a new task" />
+            </div>
+        
+            <ListGroup className="mx-auto" style={{ maxWidth: "400px" }}>
                 {ToDo.map((ToDo) => (
-                    <li id={ToDo.id}>
+                    <ListGroup.Item id={ToDo.id} className="d-flex justify-content-between align-items-center" onMouseEnter={()=> setHoveredTaskId(ToDo.id)} onMouseLeave={() => setHoveredTaskId(null)}>
                         {ToDo.text}
-                        <button onClick={() => removeToDo(ToDo.id)}>x</button>
-                    </li>
+                        <button onClick={() => removeToDo(ToDo.id)} style={{visibility: hoveredTaskId === ToDo.id ? "visible" : "hidden"}} className="btn btn-secondary btn-sm border-0">x</button>
+                    </ListGroup.Item>
                 ))}
-            </ul>
-        </div>
+            </ListGroup>
+        
+        </Container>
     )
 }
 export default ToDoList;
